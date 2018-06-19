360838307bfb591c962b8e173a22d9cb72ee4c0c
Mingjiong liao <liao0019@algonquinlive.com>
2018-06-19 11:02:14 -0400

    creat product list component

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,1 +1,16 @@
 
+class ProductList extends React.Component {
+    render() {
+      return (
+        <div className='ui unstackable items'>
+          Hello, friend! I am a basic React component.
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+  );
+  
